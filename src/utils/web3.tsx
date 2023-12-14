6c2d98134f012ec12ff6e59b81113fe0a1322ed6
scred <20541737+scredii@users.noreply.github.com>
2023-12-14 15:03:16 +0100

    squid support added

--- src/utils/web3.tsx
+++ src/utils/web3.tsx
@@ -14,6 +14,7 @@ import { switchNetwork } from "wagmi/actions";
 import { BigNumberish, ethers } from "ethers";
 import StratV5Abi from "@astrolabs/registry/abis/StrategyV5.json";
 import { getQuote } from "@astrolabs/swapper/dist/src/LiFi";
+import { BaseError, ContractFunctionRevertedError } from "viem";
 
 export const swap = async (tr: ITransactionRequestWithEstimate) => {
   if (!tr) return;
@@ -27,7 +28,7 @@ export const swap = async (tr: ITransactionRequestWithEstimate) => {
   const { hash } = await send(params);
 
   console.log("lifiExplorer: ", `https://explorer.li.fi/tx/${hash}`);
-  //   : console.log("squidExplorer: ", `https://axelarscan.io/gmp/${hash}`);
+  console.log("squidExplorer: ", `https://axelarscan.io/gmp/${hash}`);
   console.log("hash: ", hash);
   return hash;
 };
@@ -48,7 +49,7 @@ export const writeTx = async (
   abi = erc20Abi
 ) => {
   return await writeContract(
-    await prepareWriteTx(args, toAddress, functionName, abi)
+    await prepareWriteTx(args, toAddress, functionName, abi as any)
   );
 };
 
@@ -56,15 +57,29 @@ export const prepareWriteTx = async (
   args: unknown[],
   toAddress: string,
   functionName: string,
-  abi = erc20Abi
+  abi = StratV5Abi.abi
 ) => {
-  const { request } = await prepareWriteContract({
-    address: toAddress,
-    abi,
-    functionName,
-    args,
-  });
-  return request;
+  try {
+    const { request } = await prepareWriteContract({
+      address: toAddress,
+      abi: abi as any,
+      functionName,
+      args,
+    });
+    return request;
+  } catch(err) {
+    // get Wagmi custom error
+    if (err instanceof BaseError) {
+      const revertError = err.walk(err => err instanceof ContractFunctionRevertedError)
+      if (revertError instanceof ContractFunctionRevertedError) {
+        const errorName = revertError.data?.errorName ?? ''
+        console.error(errorName);
+      }
+    } else {
+      console.error(err);
+    }
+  }
+
 };
 
 export const approve = async (
@@ -83,9 +98,10 @@ export const safeWithdraw = async (
   contractAddress: string,
   amount: BigNumberish,
   receiver: string,
+  owner?: string,
+  // todo: minAmount from preview withdraw
   minAmount = "0",
   abi = StratV5Abi.abi,
-  owner?: string
 ) => {
   if (!owner) owner = receiver;
   return await writeTx(
@@ -98,29 +114,25 @@ export const safeWithdraw = async (
 
 export const withdraw = async () => {
   // todo: make it dynamic
-  const address = "0x7B56288776Cae4260770981b6BcC0f6D011C7b72";
-  const amount = ethers.parseUnits("3.30", 6);
-  const contractAddress = "0x11C8f790d252F4A49cFBFf5766310873898BF5D3";
+  const address = '0x7B56288776Cae4260770981b6BcC0f6D011C7b72';
+  const amount = ethers.parseUnits('0.30', 6);
+  const contractAddress = '0x11C8f790d252F4A49cFBFf5766310873898BF5D3';
   const chainId = 100;
   await _switchNetwork(chainId);
   const res = await safeWithdraw(contractAddress, amount, address);
+  console.log(res)
   return res;
-};
-// input: string,
-// amount: BigNumberish,
-// receiver: string,
-// minShareAmount: BigNumberish = "0",
-// params: string,
-// stratAddress: string,
-// allowance: string | number | bigint | boolean = 0n,
+}
+
+// todo: wait for smart contract to be fixed
 export const swapAndDeposit = async () => {
   const inputChainId = 100;
   const input = "0x4ecaba5870353805a9f068101a40e0f32ed605c6";
   const outputChainId = 100;
-  const output = "0xddafbb505ad214d7b80b1f830fccc89b60fb7a83";
-  const stratAddress = "0x11C8f790d252F4A49cFBFf5766310873898BF5D3";
-  const payer = "0x7B56288776Cae4260770981b6BcC0f6D011C7b72";
-  const amountWei = "1000000";
+  const output = '0xddafbb505ad214d7b80b1f830fccc89b60fb7a83';
+  const stratAddress = '0x11c8f790d252f4a49cfbff5766310873898bf5d3';
+  const payer = '0x7B56288776Cae4260770981b6BcC0f6D011C7b72';
+  const amountWei = '2000000';
   const abi = StratV5Abi.abi;
   await _switchNetwork(inputChainId);
 
@@ -143,11 +155,11 @@ export const swapAndDeposit = async () => {
   const allowance = 0n;
   if (Number(amountWei.toString()) > Number(allowance.toString())) {
     const approvalAmount = amountWei + amountWei / 500n; // 5%
-    await approve(stratAddress, approvalAmount, output);
+    await approve('0x78D5ECF1fBd052F7D8914DFBd7e3e5B5cD9aa6BB', "50000000", input);
   }
   return await writeTx(
-    "swapSafeDeposit",
-    [input, amountWei.toString(), payer, "0", params],
+    'swapSafeDeposit',
+    [input, amountWei, payer, "1000000", params],
     stratAddress,
     abi as any
   );
