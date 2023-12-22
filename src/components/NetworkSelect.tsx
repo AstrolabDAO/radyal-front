91695d62fe770655dbf5fc637b38401f1850bb71
Flow <madura.florian@gmail.com>
2023-12-22 19:27:34 +0100

    ui: update icon and network style

--- src/components/NetworkSelect.tsx
+++ src/components/NetworkSelect.tsx
@@ -8,8 +8,6 @@ import ReactSelect, {
 import { BACKGROUNDS } from "~/styles/constants";
 import { Network } from "~/utils/interfaces";
 
-
-
 const Option = ({ ...props }: OptionProps) => {
   const { network } = props.data;
   return (
@@ -25,11 +23,7 @@ const Option = ({ ...props }: OptionProps) => {
   );
 };
 
-
-
 const NetworkSelect = ({ networks, ...props }: NetworkSelectProps) => {
-  
-
   const styleOptions: StylesConfig<NetworkSelectData> = {
     control: (baseStyles: any) => {
       return {
