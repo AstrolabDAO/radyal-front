f67265756ee16ac465b939e53b79e946672641a5
Flow <madura.florian@gmail.com>
2024-01-19 11:34:51 +0100

    chore: cleaning files

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,7 +3,6 @@ import { RouteObject } from "react-router-dom";
 import SwapPage from "~/pages/SwapPage";
 import Strategies from "~/components/StrategyList";
 import HomePage from "~/pages/HomePage";
-import DisclaimerPage from "~/pages/DisclaimerPage";
 
 const routes: RouteObject[] = [
   {
